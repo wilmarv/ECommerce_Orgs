a1e0bcdd06865f5f03d2267d99049f000c4f12ff
Wilmar Vitor Fonseca <wilmarfonseca@gec.inatel.br>
2022-09-18 08:25:26 -0300

    2.2_Detalhes da cesta

--- src/view/Cesta.tsx
+++ src/view/Cesta.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Image, StyleSheet, Dimensions, Text } from "react-native";
+import { Image, StyleSheet, Dimensions, Text, View } from "react-native";
 
 const width = Dimensions.get('screen').width;
 
@@ -8,6 +8,18 @@ function Cesta() {
         <>
             <Image source={require("../../assets/topo.png")} style={styles.topo} />
             <Text style={styles.titulo}>Detalhe da cesta</Text>
+
+            <View style={styles.cesta}>
+                <Text style={styles.nome}>Cesta de verduras</Text>
+
+                <View style={styles.fazenda}>
+                    <Image style={styles.imagemFazenda} source={require("../../assets/logo.png")} />
+                    <Text style={styles.nomeFazenda}>Jenny Jack Farm</Text>
+                </View>
+
+                <Text style={styles.descricao}>Uma cesta com produtos selecionados cuidadozamente da fazenda direto para dua cozinha</Text>
+                <Text style={styles.preco}>R$ 40,00</Text>
+            </View>
 
         </>
     );
@@ -28,5 +40,40 @@ const styles = StyleSheet.create({
         color: "white",
         fontWeight: "bold",
         padding: 16
+    },
+    cesta: {
+        paddingVertical: 8,
+        paddingHorizontal: 16
+    },
+    nome: {
+        fontSize: 26,
+        lineHeight: 42,
+        color: "#464646",
+        fontWeight: "bold"
+    },
+    fazenda: {
+        flexDirection: "row",
+        paddingVertical: 12
+    },
+    imagemFazenda: {
+        width: 32,
+        height: 32
+    },
+    nomeFazenda: {
+        fontSize: 16,
+        lineHeight: 26,
+        marginLeft: 12
+    },
+    descricao: {
+        color: "#A3A3A3",
+        fontSize: 16,
+        lineHeight: 26
+    },
+    preco: {
+        color: "#2A9F85",
+        fontWeight: "bold",
+        fontSize: 26,
+        lineHeight: 42,
+        marginTop: 8
     }
 });
