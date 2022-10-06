86e3339746bc1290a8c15b1d3bd977bb660d05c2
Wilmar Vitor Fonseca <wilmarfonseca@gec.inatel.br>
2022-10-06 15:44:16 -0300

    inicializando novo projeto

--- App.tsx
+++ App.tsx
@@ -1,29 +1,9 @@
-import {
-  useFonts,
-  Montserrat_400Regular,
-  Montserrat_700Bold
-} from '@expo-google-fonts/montserrat';
-import AppLoading from 'expo-app-loading';
-import { View, StatusBar } from 'react-native';
-import Cesta from "./src/view/Cesta";
-import mock from "./src/mocks/cesta";
+import { SafeAreaView, Text } from "react-native";
 
-
-export default function App() {
-
-  const [fontCarregada] = useFonts({
-    "MontserratRegular": Montserrat_400Regular,
-    "MontserratBold": Montserrat_700Bold
-  });
-
-  if (!fontCarregada) {
-    return <AppLoading />
-  }
-
-  return (
-    <View style={{ flex: 1 }}>
-      <StatusBar backgroundColor="#000" />
-      <Cesta {...mock} />
-    </View>
-  );
+function App() {
+  return <SafeAreaView >
+    <Text>Alura</Text>
+  </SafeAreaView>
 }
+
+export default App;
--- src/interface/itens.ts
+++
@@ -1,7 +0,0 @@
-import Item from "./item";
-
-interface Itens {
-    titulo: string,
-    lista: Array<Item>
-}
-export default Itens;
